1b69ab9ce669ed1196ea4c53434e02b8a02de0fb
theAprel <cpindziak@ufl.edu>
2015-12-12 22:01:57 -0500

    Add basic log processing

--- OptionsDisplay/OptionsDisplay.cs
+++ OptionsDisplay/OptionsDisplay.cs
@@ -8,12 +8,23 @@ namespace OptionsDisplay
 {
     public class TagPrinter
     {
+        private static LogWindow window;
 
         public static void Load()
         {
-            LogWindow window = new LogWindow();
+            window = new LogWindow();
             window.Show();
-            Hearthstone_Deck_Tracker.API.LogEvents.OnPowerLogLine.Add(window.SetWindowText);
+            Hearthstone_Deck_Tracker.API.LogEvents.OnPowerLogLine.Add(processPowerLogLine);
+        }
+
+        public static void processPowerLogLine(String line)
+        {
+            if(line.Contains("tag=NUM_OPTIONS "))
+            {
+                String entity = line.Split("Entity=".ToCharArray())[1].Split(" ".ToCharArray())[0];
+                String value = line.Split("Value=".ToCharArray())[1];
+                window.SetWindowText(entity + " has " + value + " options.");
+            }
         }
     }
 }
