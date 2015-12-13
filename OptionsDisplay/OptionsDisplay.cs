de5486ea4c71fed639f8fa6401a25eebdd36ab82
theAprel <cpindziak@ufl.edu>
2015-12-12 21:42:37 -0500

    Initial commit

---
+++ OptionsDisplay/OptionsDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionsDisplay
+{
+    public class TagPrinter
+    {
+
+        public static void Load()
+        {
+            LogWindow window = new LogWindow();
+            window.Show();
+            Hearthstone_Deck_Tracker.API.LogEvents.OnPowerLogLine.Add(window.SetWindowText);
+        }
+    }
+}
---
+++ OptionsDisplay/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Controls;
+
+using Hearthstone_Deck_Tracker.Plugins;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace OptionsDisplay
+{
+    public class Plugin : IPlugin
+    {
+		public string Author
+		{
+			get { return "Aprel"; }
+		}
+
+		public string ButtonText
+		{
+			get { return "OptionsDisplay"; }
+		}
+
+		public string Description
+		{
+			get { return "Prints the NUM_OPTIONS tag to overlay."; }
+		}
+
+		public MenuItem MenuItem
+		{
+			get { return null; }
+		}
+
+		public string Name
+		{
+			get { return "OptionsDisplay"; }
+		}
+
+		public void OnButtonPress()
+		{
+		}
+
+		public void OnLoad()
+		{
+            TagPrinter.Load();
+		}
+
+		public void OnUnload()
+		{
+		}
+
+		public void OnUpdate()
+		{
+		}
+
+		public Version Version
+		{
+			get { return new Version(0, 0, 1); }
+		}
+    }
+}
